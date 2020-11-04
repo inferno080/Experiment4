9fee25a858c2aa669ee3577ddb7a70c0db33792a
inferno080 <67534207+inferno080@users.noreply.github.com>
2020-11-04 23:52:51 +0530

    Hry

--- thejs.js
+++ thejs.js
@@ -100,18 +100,9 @@ function updatevalueondisplay()
                 y:[pH],
                 type:'line'
             }]);
-            var cnt = 0;
             setInterval(function(){
-                Plotly.extendTraces('chart',{ y:[[pH]]}, [V2]);
-                cnt++;
-                if(cnt > 500) {
-                    Plotly.relayout('chart',{
-                        xaxis: {
-                            range: [cnt-500,cnt]
-                        }
-                    });
-                }
-},15); 
+                Plotly.extendTraces('chart',{ y:[[pH]]}, [0]);
+            },700); 
             final = pH
     }
     else
